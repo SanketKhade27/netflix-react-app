f445a0e8a0246c5221fcee303dfb7a31f6597a9d
Sanket Khade <sanketkhade4848.sk@gmail.com>
2021-08-29 23:26:56 +0530

    Third Commit - React Pure Slider

--- netflix-app/src/components/featured/Featured.jsx
+++ netflix-app/src/components/featured/Featured.jsx
@@ -1,32 +1,58 @@
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
 import "./featured.scss";
 
-const Featured = () => {
-    return (
-        <div className="featured">
-            <img src="https://images.unsplash.com/photo-1502389343615-b07268585dae?ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8Mzl8MjQzNzc2Mnx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
-            <div className="info">
-                <img src="https://wallpapercave.com/wp/ydhMobg.jpg" alt="" />
-                <span className="desc">
-                    lorem ipsum dolor sit amet, consectetur adip
-                    lorem ipsum dolor sit amet, consectetur adip
-                    lorem ipsum dolor sit amet, consectetur adip
-                    lorem ipsum dolor sit amet, consectetur adip
-                    lorem ipsum dolor sit amet, consectetur adip
-                </span>
-                <div className="buttons">
-                    <button className="play">
-                        <PlayArrow />
-                        <span>Play</span>
-                    </button>
-                    <button className="more">
-                        <InfoOutlined />
-                        <span>Info</span>
-                    </button>
-                </div>
-            </div>
+const Featured = ({ type }) => {
+  return (
+    <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
         </div>
-    )
-}
+      )}
+      <img
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      <div className="info">
+        <img
+          src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
+          alt=""
+        />
+        <span className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          adipisci repellendus eum quasi illo, velit numquam, maxime tempora
+          sint deleniti, aliquid qui? Facilis, adipisci! Ratione hic repudiandae
+          temporibus eum earum?
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
